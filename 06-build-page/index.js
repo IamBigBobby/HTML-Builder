7dc257d95964f2dae08c9cf94ed1866b9f529158
Denis Svetleishii <denissvetleishii@outlook.com>
2024-01-23 20:36:53 +0400

    feat: add final solution 06-build-page

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -1,64 +1,126 @@
 const fs = require('fs');
 const path = require('path');
 
-const arrBundleCss = [];
-const assetsDirectory = path.join(__dirname, 'assets');
-const assetsCopyDirectory = path.join(__dirname, 'project-dist', 'assets');
-const teamplateHtml = path.join(__dirname, 'template.html');
-const indexHtml = path.join(__dirname, 'project-dist', 'index.html');
-const styleCss = path.join(__dirname, 'project-dist', 'style.css');
+let arrBundleCss = [];
+let assetsDirectory = path.join(__dirname, 'assets');
+let assetsCopyDirectory = path.join(__dirname, 'project-dist/assets');
+let teamplateHtml = path.join(__dirname, 'template.html');
+let indexHtml = path.join(__dirname, 'project-dist/index.html');
+
+// writeStream
+const outBundleCss = fs.createWriteStream(
+  path.join(__dirname, '/project-dist', 'style.css'),
+  (err) => {
+    if (err) throw err;
+  },
+);
 
 // create directories
-fs.mkdirSync(path.join(__dirname, 'project-dist'), { recursive: true });
-fs.mkdirSync(path.join(__dirname, 'project-dist/assets'), { recursive: true });
+fs.mkdir(path.join(__dirname, 'project-dist'), { recursive: true }, (err) => {
+  if (err) throw err;
+  fs.mkdir(
+    path.join(__dirname, 'project-dist/assets'),
+    { recursive: true },
+    (err) => {
+      if (err) throw err;
+      // create files
+      fs.writeFile(indexHtml, '', (err) => {
+        if (err) throw err;
 
-// create files
-fs.writeFileSync(path.join(__dirname, 'project-dist', 'index.html'), '');
+        // merge styles
+        fs.readdir(
+          path.join(__dirname, 'styles'),
+          { withFileTypes: true },
+          (err, files) => {
+            if (err) throw err;
 
-// merge styles
-fs.readdirSync(path.join(__dirname, 'styles')).forEach((file) => {
-  let styleFile = path.join(__dirname, 'styles', file);
+            files.forEach((file) => {
+              let styleFile = path.join(__dirname, 'styles', file.name);
 
-  if (fs.statSync(styleFile).isFile() && path.extname(styleFile) === '.css') {
-    let cssContent = fs.readFileSync(styleFile, 'utf8');
-    arrBundleCss.push(cssContent);
-  }
-});
+              if (file.isFile() && path.extname(styleFile) === '.css') {
+                let stream = fs.createReadStream(styleFile, 'utf8');
 
-// write bundled styles to file
-fs.writeFileSync(styleCss, arrBundleCss.join('').trim());
+                stream.on('data', (chunk) => {
+                  arrBundleCss.push(chunk);
+                });
 
-// fill index
-fs.copyFileSync(teamplateHtml, indexHtml);
-let templateData = fs.readFileSync(teamplateHtml, 'utf-8');
-let sections = templateData.match(/{{\w+}}/gm);
+                stream.on('error', (err) => {
+                  if (err) throw err;
+                });
 
-sections.forEach((section) => {
-  let sectionTemplate = path.join(
-    __dirname,
-    'components',
-    `${section.slice(2, -2)}.html`,
+                stream.on('end', () => {
+                  outBundleCss.write(arrBundleCss.pop().trim());
+                });
+              }
+            });
+          },
+        );
+
+        // fill index
+        fs.copyFile(teamplateHtml, indexHtml, (err) => {
+          if (err) throw err;
+
+          fs.readFile(teamplateHtml, 'utf-8', (err, data) => {
+            if (err) throw err;
+
+            let templateData = data;
+            let sections = data.match(/{{\w+}}/gm);
+
+            sections.forEach((section) => {
+              let sectionTemplate = path.join(
+                __dirname,
+                'components',
+                `${section.slice(2, -2)}.html`,
+              );
+
+              fs.readFile(sectionTemplate, 'utf-8', (err, sectionTagData) => {
+                if (err) throw err;
+
+                templateData = templateData.replace(section, sectionTagData);
+
+                fs.rm(indexHtml, { recursive: true }, (err) => {
+                  if (err) throw err;
+
+                  let indexStream = fs.createWriteStream(indexHtml);
+                  indexStream.write(templateData);
+                });
+              });
+            });
+          });
+        });
+      });
+    },
   );
-
-  let sectionTagData = fs.readFileSync(sectionTemplate, 'utf-8');
-  templateData = templateData.replace(section, sectionTagData);
 });
-
-fs.writeFileSync(indexHtml, templateData);
 
 // copy assets
 function copyAssets(directory, copyDirectory) {
-  fs.readdirSync(directory).forEach((file) => {
-    let pathDir = path.join(directory, file);
-    let pathDirCopy = path.join(copyDirectory, file);
+  fs.readdir(path.join(directory), { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
 
-    if (fs.statSync(pathDir).isFile()) {
-      fs.copyFileSync(pathDir, pathDirCopy);
-    }
-    if (fs.statSync(pathDir).isDirectory()) {
-      fs.mkdirSync(pathDirCopy, { recursive: true });
-      copyAssets(pathDir, pathDirCopy);
-    }
+    files.forEach((file) => {
+      let pathDir = path.join(directory, file.name);
+      let pathDirCopy = path.join(copyDirectory, file.name);
+
+      if (file.isFile()) {
+        fs.access(pathDir, fs.constants.F_OK, (err) => {
+          if (err) {
+            console.error(`File not found: ${pathDir}`);
+            return;
+          }
+
+          fs.copyFile(pathDir, pathDirCopy, (err) => {
+            if (err) throw err;
+          });
+        });
+      }
+      if (file.isDirectory()) {
+        fs.mkdir(pathDirCopy, { recursive: true }, (err) => {
+          if (err) throw err;
+        });
+        copyAssets(pathDir, pathDirCopy);
+      }
+    });
   });
 }
 
