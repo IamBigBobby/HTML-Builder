403b8b666bdb5b457467b55a640a249223c1f07d
Denis Svetleishii <denissvetleishii@outlook.com>
2024-01-23 21:01:47 +0400

    refactor: delete consloe.log

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,19 +3,15 @@ const path = require('path');
 
 const copyFolderPath = path.join(__dirname, 'copy-files');
 
-// Удаление предыдущей версии папки
 fs.rm(copyFolderPath, { recursive: true, force: true }, (err) => {
   if (err && err.code !== 'ENOENT') {
-    // Если произошла ошибка, кроме того, что папка не существует (ENOENT), выбросить ошибку
     throw err;
   }
 
-  // Создание новой папки после удаления предыдущей
   fs.mkdir(copyFolderPath, { recursive: true }, (err) => {
     if (err) throw err;
     console.log('Folder has been created');
 
-    // Копирование файлов
     fs.readdir(
       path.join(__dirname, 'files'),
       { withFileTypes: true },
@@ -28,7 +24,6 @@ fs.rm(copyFolderPath, { recursive: true, force: true }, (err) => {
             path.join(copyFolderPath, file.name),
             (err) => {
               if (err) throw err;
-              console.log(`${file.name}`);
             },
           );
         });
